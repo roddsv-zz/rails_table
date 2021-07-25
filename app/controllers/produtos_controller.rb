bf2e961c0a73fc318e29ec8fa3bc9b2650793772
Rodrigo Soares <adsrodsoares@gmail.com>
2021-07-25 12:18:35 -0300

    Using interface to create new products

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,7 +1,13 @@
 class ProdutosController < ApplicationController
 
     def index
-        @produtos = Produto.order(nome: :desc).limit 2
-        @produto_com_desconto = Produto.order(:preco).limit 1
+        @produtos = Produto.order(nome: :desc).limit 5
+        @produto_com_desconto = Produto.order(:preco).limit 5
+    end
+
+    def create
+        produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+        Produto.create produto
+        redirect_to root_path
     end
 end
